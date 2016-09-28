0be4c79b433f3dff882ac0778d4482ffb490ee1b
Yadwinder <ryker1990@gmail.com>
2016-09-28 18:36:58 +0530

    Update

---
+++ ESP8266/BH1715.ino
@@ -0,0 +1,109 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// BH1715
+// This code is designed to work with the BH1715_I2CS I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Light?sku=BH1715_I2CS#tabs-0-product_tabset-2/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <Wire.h>
+
+// BH1715 I2C address is 0x23(35)
+#define Addr 0x23
+
+const char* ssid = "your ssid network";
+const char* password = "your password";
+float luminance;
+
+ESP8266WebServer server(80);
+
+void handleroot()
+{
+  unsigned int data[2];
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Send power on command
+  Wire.write(0x01);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Send continuous measurement command
+  Wire.write(0x10);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 2 byte of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // luminance msb, luminance lsb
+  if (Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+
+  // Convert the data
+  luminance = ((data[0] * 256) + data[1]) / 1.20;
+
+  // Output data to serial monitor
+  Serial.print("Ambient Light Luminance :");
+  Serial.print(luminance);
+  Serial.println(" lux");
+
+  // Output data to web server
+  server.sendContent
+  ("<html><head><meta http-equiv='refresh' content='3'</meta>"
+   "<h1 style=text-align:center;font-size:300%;color:blue;font-family:britannic bold;>CONTROL EVERYTHING</h1>"
+   "<h3 style=text-align:center;font-family:courier new;><a href=http://www.controleverything.com/ target=_blank>www.controleverything.com</a></h3><hr>"
+   "<h2 style=text-align:center;font-family:tahoma;><a href=https://www.controleverything.com/content/Light?sku=BH1715_I2CS#tabs-0-product_tabset-2/ \n"
+   "target=_blank>BH1715 Sensor I2C Mini Module</a></h2>");
+  server.sendContent
+  ("<h3 style=text-align:center;font-family:tahoma;>Ambient Light Luminance = " + String(luminance) + " lux");
+}
+
+void setup()
+{
+  // Initialise I2C communication as MASTER
+  Wire.begin(2, 14);
+  // Initialise serial communication, set baud rate = 115200
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  // Get the IP address of ESP8266
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start the server
+  server.on("/", handleroot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop()
+{
+  server.handleClient();
+}
+
